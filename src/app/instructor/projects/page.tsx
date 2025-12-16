7c5f202823f174acae789acc23a858d89aa3f863
elzizzah <chukwudiokolo416@gmail.com>
2025-12-16 15:52:08 +0100

    'lol'

--- src/app/instructor/projects/page.tsx
+++ src/app/instructor/projects/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic';
 
 import { useState, useEffect } from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
